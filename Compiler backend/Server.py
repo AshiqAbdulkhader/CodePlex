5e64b3975d5c30d939064e55d6016d6c2626dd48
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2021-08-15 11:42:48 +0530

    Create Server.py

---
+++ Compiler backend/Server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from compylex.compiler import Compile
+import json
+
+
+class Item(BaseModel):
+    code: str
+    lang: str
+    input: str
+    id: int
+
+
+app = FastAPI()
+
+
+@app.post("/compile/")
+async def create_item(item: Item):
+    result = Compile(item.code, item.lang, item.input, item.id)
+    output = result.get_output()
+    data = {
+        'output': output,
+        'status': result.get_status()
+    }
+    data = json.dumps(data)
+    return data
