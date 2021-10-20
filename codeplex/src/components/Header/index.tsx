187e1cdd610239cf01cc8094f5668a266cf8b8cf
Suhail Ahmed <61581649+SuhailAhmedVelorum@users.noreply.github.com>
2021-10-20 09:43:27 +0530

    removed broken import

--- codeplex/src/components/Header/index.tsx
+++ codeplex/src/components/Header/index.tsx
@@ -2,43 +2,42 @@ import React from "react";
 
 //Assets
 import '../../fonts.css';
-import './active-ui.css';
 import CodePlexLogo from "../../images/logo.svg";
 import CodePlexText from "../../images/Codeplex.svg";
 import Search from "../../images/search.svg";
 
 //Components
-import {Wrapper, Container,  LogoImg, LogoText, HeaderLink, SearchButton, SearchImg} from './Header.styles';
+import { Wrapper, Container, LogoImg, LogoText, HeaderLink, SearchButton, SearchImg } from './Header.styles';
 import { GenericButton } from "../GenericButton/GenericButton.styles";
-import {LinkContainer} from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header: React.FC = () => (
-		<Wrapper>
-				<LinkContainer to='/'>
-					<Container>
-						<LogoImg src={CodePlexLogo} alt="logo"/>
-						<LogoText src={CodePlexText} alt="logo-text"/>
-					</Container>
-				</LinkContainer>
-				<LinkContainer to='/features'>
-					<HeaderLink>Features</HeaderLink>
-				</LinkContainer>
-				<LinkContainer to='/about'>
-					<HeaderLink>About</HeaderLink>
-				</LinkContainer>
-				<LinkContainer to='/contact'>
-					<HeaderLink>Contact</HeaderLink>
-				</LinkContainer>
-				<LinkContainer to='/contributors'>
-					<HeaderLink>Contributors</HeaderLink>
-				</LinkContainer>
-				<SearchButton>
-					<SearchImg src={Search} alt="Search button"/>
-				</SearchButton>
-				<LinkContainer to='/login'>
-					<GenericButton>Sign In</GenericButton>
-				</LinkContainer>
-		</Wrapper>
-	);
+	<Wrapper>
+		<LinkContainer to='/'>
+			<Container>
+				<LogoImg src={CodePlexLogo} alt="logo" />
+				<LogoText src={CodePlexText} alt="logo-text" />
+			</Container>
+		</LinkContainer>
+		<LinkContainer to='/features'>
+			<HeaderLink>Features</HeaderLink>
+		</LinkContainer>
+		<LinkContainer to='/about'>
+			<HeaderLink>About</HeaderLink>
+		</LinkContainer>
+		<LinkContainer to='/contact'>
+			<HeaderLink>Contact</HeaderLink>
+		</LinkContainer>
+		<LinkContainer to='/contributors'>
+			<HeaderLink>Contributors</HeaderLink>
+		</LinkContainer>
+		<SearchButton>
+			<SearchImg src={Search} alt="Search button" />
+		</SearchButton>
+		<LinkContainer to='/login'>
+			<GenericButton>Sign In</GenericButton>
+		</LinkContainer>
+	</Wrapper>
+);
 
 export default Header;
